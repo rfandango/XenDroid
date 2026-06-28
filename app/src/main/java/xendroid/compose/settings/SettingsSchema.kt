f65a6285976f3e100bc5add6b71c9929a3f4a587
Fabxx <30447649+Fabxx@users.noreply.github.com>
2026-06-28 13:32:31 +0200

    Add option toggle in UI

--- app/src/main/java/xendroid/compose/settings/SettingsSchema.kt
+++ app/src/main/java/xendroid/compose/settings/SettingsSchema.kt
@@ -182,6 +182,7 @@ object SettingsSchema {
             l("GPU", "gpu", "GPU backend", "vulkan",
                 "vulkan" to "vulkan", "null" to "null"),
             b("GPU", "depth_float24_convert_in_pixel_shader", "Depth float24 convert in pixel shader", false),
+            b("GPU", "value_convert_7e3_8888_reuse", "Resolve scale: fill half-pixel offset", true),
         )),
 
         SettingsCategory("CPU", listOf(
